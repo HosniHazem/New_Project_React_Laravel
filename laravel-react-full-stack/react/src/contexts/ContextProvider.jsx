b1f07ef478d06e93d892d2f921c7cd72ca01a6c5
HosniHazem <hazem.hosni@aiesec.net>
2023-03-12 19:07:49 +0100

    Backend Edit + Last Front End Edit

--- laravel-react-full-stack/react/src/contexts/ContextProvider.jsx
+++ laravel-react-full-stack/react/src/contexts/ContextProvider.jsx
@@ -1,17 +1,18 @@
 import {createContext, useContext, useState} from "react";
 
 const StateContext = createContext({
-  user: null,
+  currentUser: null,
   token: null,
+  notification: null,
   setUser: () => {},
-  setToken: () => {}
+  setToken: () => {},
+  setNotification: () => {}
 })
 
 export const ContextProvider = ({children}) => {
-  const [user, setUser] = useState({
-    name: 'jhon'
-  });
+  const [user, setUser] = useState({});
   const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+  const [notification, _setNotification] = useState('');
 
   const setToken = (token) => {
     _setToken(token)
@@ -22,13 +23,22 @@ export const ContextProvider = ({children}) => {
     }
   }
 
+  const setNotification = message => {
+    _setNotification(message);
+
+    setTimeout(() => {
+      _setNotification('')
+    }, 5000)
+  }
 
   return (
     <StateContext.Provider value={{
       user,
       setUser,
       token,
-      setToken
+      setToken,
+      notification,
+      setNotification
     }}>
       {children}
     </StateContext.Provider>
--- laravel-react-full-stack/routes/api.php
+++ laravel-react-full-stack/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +14,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::apiResource('/users', UserController::class);
 });
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
